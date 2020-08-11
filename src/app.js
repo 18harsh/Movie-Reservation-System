9acc72a83d40990ac3023244f03bf08a12f9a9a3
Harsh Gandhi <18harshgandhi@gmail.com>
2020-08-11 11:42:40 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectionPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectionPath))
+
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000,()=> {
+    console.log('Server is up on port 3000.')
+})
