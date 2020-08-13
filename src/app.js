415b673ca3ef4d38e2023cd5c7aac2e03923d083
Harsh Gandhi <18harshgandhi@gmail.com>
2020-08-13 15:35:33 +0530

    login

--- src/app.js
+++ src/app.js
@@ -22,6 +22,10 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
 app.get('*', (req,res) => {
     res.render('404', {
         errorMessage: 'Page not found',
